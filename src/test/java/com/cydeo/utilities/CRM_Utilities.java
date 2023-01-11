9e04a9860309fd55ca7583a9ba5bf93808747d30
Halid Erdur <haliderdur@gmail.com>
2023-01-11 11:16:52 +0000

    CRM_login utility methods

---
+++ src/test/java/com/cydeo/utilities/CRM_Utilities.java
@@ -0,0 +1,51 @@
+package com.cydeo.utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CRM_Utilities {
+
+    /*
+    "CRM_login" method will login with using helpdesk1@cybertekschool.com
+    user when its called
+     */
+    public static void crm_login(WebDriver driver) {
+        /*
+        Credentials:
+            helpdesk1@cybertekschool.com  UserUser
+            Helpdesk2@cybertekschool.com  UserUser
+         */
+
+        //3. Enter valid username
+        WebElement inputUserName = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        inputUserName.sendKeys("helpdesk1@cybertekschool.com");
+
+        //4. Enter valid password
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        inputPassword.sendKeys("UserUser");
+
+        //5. Click to `Log In` button
+        WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+    }
+
+    public static void crm_login(WebDriver driver, String username, String password) {
+        /*
+        Credentials:
+            helpdesk1@cybertekschool.com  UserUser
+            Helpdesk2@cybertekschool.com  UserUser
+         */
+        //3. Enter valid username
+        WebElement inputUserName = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        inputUserName.sendKeys(username);
+
+        //4. Enter valid password
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        inputPassword.sendKeys(password);
+
+        //5. Click to `Log In` button
+        WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+    }
+}
