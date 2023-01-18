774a0326c6c41445bedd7b774c822a858e6d61be
Halid Erdur <haliderdur@gmail.com>
2023-01-18 10:40:34 +0000

    TestBase update

--- src/test/java/com/cydeo/base/TestBase.java
+++ src/test/java/com/cydeo/base/TestBase.java
@@ -17,7 +17,6 @@ public abstract class TestBase {
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
     }
 
     @AfterMethod
